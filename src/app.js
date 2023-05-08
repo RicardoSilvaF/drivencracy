162478c8ee084d26621809999aa43fd6f0f7967d
RicardoSilvaF <zanphs@gmail.com>
2023-05-08 08:02:49 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/routes.js";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`servidor ligado na porta ${PORT}`));
